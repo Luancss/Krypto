2e418c1c44ea69e42b7f2ce9088c7600427c35b8
Luancss <ventobo123@gmail.com>
2023-06-08 02:08:18 -0300

    Marque Section

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active')
   navUl.classList.toggle('active')
 })
+
+const root = document.documentElement;
+const marqueElementsDisplayed = getComputedStyle(root).getPropertyValue('--marque-elements-displayed');
+
+const marqueContent = document.querySelector('.marque-content');
+
+root.style.setProperty('--marque-elements', marqueContent.children.length);
+
+for (let i = 0; i < marqueElementsDisplayed; i++) {
+    marqueContent.appendChild(marqueContent.children[i].cloneNode(true))
+}
