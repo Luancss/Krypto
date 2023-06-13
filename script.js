aed36661b97aa93619516d41a331fc36f41489c4
Luancss <ventobo123@gmail.com>
2023-06-13 00:03:58 -0300

    Parcerias Desktop

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ root.style.setProperty('--marque-elements', marqueContent.children.length);
 for (let i = 0; i < marqueElementsDisplayed; i++) {
     marqueContent.appendChild(marqueContent.children[i].cloneNode(true))
 }
+
+
+const marqueAni = document.getElementById('marque-ani')
+const marqueFixed = document.getElementById('marque-fixed')
+
+function screenChange(e) {
+    if(e.matches) {
+        marqueAni.classList.add('hide');
+        marqueFixed.classList.remove('hide');
+    } else { 
+        marqueFixed.classList.add('hide');
+        marqueAni.classList.remove('hide');
+    }
+}
+
+const mediaQuery = window.matchMedia('(min-width: 800px)')
+
+mediaQuery.addListener(screenChange)
+
+screenChange(mediaQuery)
